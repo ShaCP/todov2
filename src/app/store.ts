9fac268b380bb3845a107349319bf4941d0a1757
ShaCP <srcp2007@gmail.com>
2024-01-13 14:17:28 -0500

    Rename of files and variables (from names in template)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todosReducer from "../features/counter/counterSlice";
+import todosReducer from "../features/todos/todosSlice";
 
 export const store = configureStore({ reducer: todosReducer });
 
 // import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
+// import todosReducer from '../features/todos/todosSlice';
 
 // export const store = configureStore({
 //   reducer: {
-//     counter: counterReducer,
+//     todos: todosReducer,
 //   },
 // });
 
--- src/features/todos/Todo.tsx
+++ src/features/todos/Todo.tsx
@@ -6,9 +6,9 @@ import {
   selectTodos,
   addWithMessage,
   removeWithMessage
-} from "./counterSlice";
+} from "./todosSlice";
 import { Toaster } from "react-hot-toast";
-import "./Counter.module.css";
+import "./Todos.module.css";
 
 export const Todos = () => {
   const todos = useSelector(selectTodos);
@@ -92,71 +92,3 @@ export const Todos = () => {
     </>
   );
 };
-
-// import React, { useState } from "react";
-
-// import { useAppSelector, useAppDispatch } from "../../app/hooks";
-// import {
-//   decrement,
-//   increment,
-//   incrementByAmount,
-//   incrementAsync,
-//   incrementIfOdd,
-//   selectCount
-// } from "./counterSlice";
-// import styles from "./Counter.module.css";
-// export function Counter() {
-//   const count = useAppSelector(selectCount);
-//   const dispatch = useAppDispatch();
-//   const [incrementAmount, setIncrementAmount] = useState('2');
-
-//   const incrementValue = Number(incrementAmount) || 0;
-
-//   return (
-//     <div>
-//       <div className={styles.row}>
-//         <button
-//           className={styles.button}
-//           aria-label="Decrement value"
-//           onClick={() => dispatch(decrement())}
-//         >
-//           -
-//         </button>
-//         <span className={styles.value}>{count}</span>
-//         <button
-//           className={styles.button}
-//           aria-label="Increment value"
-//           onClick={() => dispatch(increment())}
-//         >
-//           +
-//         </button>
-//       </div>
-//       <div className={styles.row}>
-//         <input
-//           className={styles.textbox}
-//           aria-label="Set increment amount"
-//           value={incrementAmount}
-//           onChange={(e) => setIncrementAmount(e.target.value)}
-//         />
-//         <button
-//           className={styles.button}
-//           onClick={() => dispatch(incrementByAmount(incrementValue))}
-//         >
-//           Add Amount
-//         </button>
-//         <button
-//           className={styles.asyncButton}
-//           onClick={() => dispatch(incrementAsync(incrementValue))}
-//         >
-//           Add Async
-//         </button>
-//         <button
-//           className={styles.button}
-//           onClick={() => dispatch(incrementIfOdd(incrementValue))}
-//         >
-//           Add If Odd
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
